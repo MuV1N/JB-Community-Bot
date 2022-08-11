6ab2de71e97e4bb3596665b067a1a2e2f4d1b1e3
MuV1N <75033565+MuV1N@users.noreply.github.com>
2022-08-11 17:01:39 +0200

    Removed usless imports TextChannel variable and not in the code made new EmbedBuilder variables

--- src/main/java/de/muv1n/theRealBot/slashCommands/BanCommand.java
+++ src/main/java/de/muv1n/theRealBot/slashCommands/BanCommand.java
@@ -2,52 +2,42 @@ package de.muv1n.theRealBot.slashCommands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.Command;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class BanCommand extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent e) {
+
+        TextChannel logs = e.getGuild().getTextChannelsByName("\uD83D\uDCD8┃logs", false).get(0);
+
         if (!e.getName().equals("ban"))return;
         User user = Objects.requireNonNull(e.getOption("name")).getAsUser();
-        EmbedBuilder eor = new EmbedBuilder();
-        eor.setColor(Color.RED);
-        eor.setTitle(":hammer:Erfolgreich gebannt:hammer:");
-        eor.addField("Der Nutzer " + user.getName() + " wurde gebannt!", "", false);
+
         if (e.getOption("begründung") != null){
         String reason = Objects.requireNonNull(e.getOption("begründung")).getAsString();
-        EmbedBuilder er = new EmbedBuilder();
-        er.setTitle(":hammer:Erfolgreich gebannt:hammer:");
-        er.setColor(Color.RED);
-        er.addField("Der Nutzer " + user.getName() + " wurde gebannt!", "", false);
-        er.addField("Die Begründung ist:", "", false);
-        er.addField("**" + reason + "**", "", false);
         sendPrivateMessageWithReason(user, e.getGuild(), reason);
         Objects.requireNonNull(e.getGuild()).ban(user, 1, reason).queue();
-        e.replyEmbeds(er.build()).setEphemeral(true).queue();
-            e.getGuild().getTextChannelsByName("\uD83D\uDCD8logs", false).get(0).sendMessageEmbeds(er.build()).queue();
+        e.replyEmbeds(sendEmbedWithReason(user, reason, e).build()).setEphemeral(true).queue();
+            logs.sendMessageEmbeds(sendEmbedWithReason(user, reason, e).build()).queue();
         }else{
-            e.getGuild().getTextChannelsByName("\uD83D\uDCD8logs", false).get(0).sendMessageEmbeds(eor.build()).queue();
+            logs.sendMessageEmbeds(sendEmbedWithoutReason(user, e).build()).queue();
             sendPrivateMessageWithoutReason(user, e.getGuild());
             Objects.requireNonNull(e.getGuild()).ban(user, 1).queue();
-            e.replyEmbeds(eor.build()).setEphemeral(true).queue();
+            e.replyEmbeds(sendEmbedWithoutReason(user, e).build()).setEphemeral(true).queue();
         }
     }
     public void sendPrivateMessageWithReason(User user, Guild guild, String reason){
         EmbedBuilder eb = new EmbedBuilder();
         eb.setColor(Color.RED);
-        eb.setTitle(":hammer:Du wurdest gebannt:sob:");
+        eb.setTitle(":hammer:Du wurdest gebannt:hammmer:");
         eb.addField("Die Begründung ist:", "", true);
         eb.addField("**" + reason + "**", "", false);
         user.openPrivateChannel().flatMap((privateChannel -> privateChannel.sendMessageEmbeds(eb.build()))).queue();
@@ -58,5 +48,21 @@ public class BanCommand extends ListenerAdapter {
         eb.setTitle(":hammer:Du wurdest gebannt:sob:");
         eb.addField("Es gibt keine Begründung!", "", true);
         user.openPrivateChannel().flatMap((privateChannel -> privateChannel.sendMessageEmbeds(eb.build()))).queue();
+    }
+    public EmbedBuilder sendEmbedWithReason(User user, String reason, SlashCommandInteractionEvent e){
+        EmbedBuilder er = new EmbedBuilder();
+        er.setTitle(":hammer:Erfolgreich gebannt:hammer:");
+        er.setColor(Color.RED);
+        er.addField("Der Nutzer " + user.getName() + " wurde von " + Objects.requireNonNull(e.getMember()).getAsMention() + " gebannt!", "", false);
+        er.addField("Die Begründung ist:", "", false);
+        er.addField("**" + reason + "**", "", false);
+        return er;
+    }
+    public EmbedBuilder sendEmbedWithoutReason(User user, SlashCommandInteractionEvent e){
+        EmbedBuilder er = new EmbedBuilder();
+        er.setTitle(":hammer:Erfolgreich gebannt:hammer:");
+        er.setColor(Color.RED);
+        er.addField("Der Nutzer " + user.getAsMention() + " wurde von " + Objects.requireNonNull(e.getMember()).getAsMention() + " gebannt!", "", false);
+        return er;
     }
 }
